934e94d5587f1e0dba618c068aee558c7d2c9188
fbilhac <flavie.bilhac@gmail.com>
2020-03-03 11:47:32 +0100

    init

---
+++ src/main/java/com/isis/adventureISISServer/adventureISISServer/AdventureIsisServerApplication.java
@@ -0,0 +1,13 @@
+package com.isis.adventureISISServer.adventureISISServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureIsisServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventureIsisServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isis/adventureISISServer/adventureISISServer/AdventureIsisServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isis.adventureISISServer.adventureISISServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureIsisServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
