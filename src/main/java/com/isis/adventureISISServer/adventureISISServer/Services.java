bacd60f36ff8e775f4319d927c26328ae453320c
fbilhac <flavie.bilhac@gmail.com>
2020-03-11 12:30:08 +0100

    modifs page 42

--- src/main/java/com/isis/adventureISISServer/adventureISISServer/Services.java
+++ src/main/java/com/isis/adventureISISServer/adventureISISServer/Services.java
@@ -5,6 +5,8 @@
  */
 package com.isis.adventureISISServer.adventureISISServer;
 
+import generated.PallierType;
+import generated.ProductType;
 import generated.World;
 import java.io.File;
 import java.io.FileInputStream;
@@ -31,12 +33,10 @@ public class Services {
         InputStream input;
         try {
             try {
-            File f = new File(pseudo + "world.xml");
-    
-            input = new FileInputStream(f);
-            }
-            catch (Exception e) {
-                
+                File f = new File(pseudo + "world.xml");
+                input = new FileInputStream(f);
+            } catch (Exception e) {
+
                 input = getClass().getClassLoader().getResourceAsStream("world.xml");
                 System.out.println("pas de monde associé au joueur" + e.getMessage());
             }
@@ -67,5 +67,93 @@ public class Services {
     public World getWorld(String pseudo) {
         return readWorldFromXml(pseudo);
     }
+    // prend en paramètre le pseudo du joueur et le produit
+    // sur lequel une action a eu lieu (lancement manuel de production ou
+    // achat d’une certaine quantité de produit)
+    // renvoie false si l’action n’a pas pu être traitée
+
+    public Boolean updateProduct(String username, ProductType newproduct) {
+        // aller chercher le monde qui correspond au joueur
+        World world = getWorld(username);
+
+        // trouver dans ce monde, le produit équivalent à celui passé
+        // en paramètre
+        ProductType product = findProductById(world, newproduct.getId());
+        if (product == null) {
+            return false;
+        }
+
+        // calculer la variation de quantité. Si elle est positive c'est
+        // que le joueur a acheté une certaine quantité de ce produit
+        // sinon c’est qu’il s’agit d’un lancement de production.
+        int qtchange = newproduct.getQuantite() - product.getQuantite();
+        if (qtchange > 0) {
+            // soustraire de l'argent du joueur le cout de la quantité achetée 
+            //et mettre à jour la quantité de product
+            double cout = (product.getCout() * (1 - Math.pow(product.getCroissance(), product.getQuantite()))) / (1 - product.getCroissance());
+            world.setMoney(world.getMoney() - cout);
+            product.setQuantite(product.getQuantite() + newproduct.getQuantite());
+
+        } else {
+            // initialiser product.timeleft à product.vitesse pour lancer la production
+            product.setTimeleft(product.getVitesse());
+            product.setQuantite(newproduct.getQuantite());
+        }
+        // sauvegarder les changements du monde
+        saveWorldToXml(world, username);
+        return true;
+    }
+    // prend en paramètre le pseudo du joueur et le manager acheté.
+    // renvoie false si l’action n’a pas pu être traitée
+
+    public Boolean updateManager(String username, PallierType newmanager) {
+        // aller chercher le monde qui correspond au joueur
+        World world = getWorld(username);
+        // trouver dans ce monde, le manager équivalent à celui passé
+        // en paramètre
+        PallierType manager = findManagerByName(world, newmanager.getName());
+        //Page 42
+        if (manager == null) {
+            return false;
+        }
+
+        // débloquer ce manager
+        // trouver le produit correspondant au manager
+        ProductType product = findProductById(world, manager.getIdcible());
+        if (product == null) {
+            return false;
+        }
+        // débloquer le manager de ce produit
+        product.setManagerUnlocked(true);
+        manager.setUnlocked(true);
+        // soustraire de l'argent du joueur le cout du manager
+        world.setMoney(world.getMoney()-manager.getSeuil());
+        // sauvegarder les changements au monde
+        saveWorldToXml(world,username);
+        return true;
+    }
+
+    private ProductType findProductById(World world, int id) {
+        ProductType pt = null;
+        for (ProductType p : world.getProducts().getProduct()) {
+            if (p.getId() == id) {
+                pt = p;
+            }
+        }
+        return pt;
+
+    }
+
+    private PallierType findManagerByName(World world, String name) {
+        PallierType palt = null;
+        for (PallierType pal : world.getManagers().getPallier()) {
+            if (pal.getName().equals(name)) {
+                palt = pal;
+            }
+        }
+        return palt;
+    }
+
+ 
 
 }
--- src/main/java/com/isis/adventureISISServer/adventureISISServer/WebServices.java
+++ src/main/java/com/isis/adventureISISServer/adventureISISServer/WebServices.java
@@ -11,9 +11,15 @@ package com.isis.adventureISISServer.adventureISISServer;
  */
 
 
+import com.google.gson.Gson;
+import generated.PallierType;
+import generated.ProductType;
 import javax.servlet.http.HttpServletRequest;
 import static javax.swing.text.html.FormSubmitEvent.MethodType.GET;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import static javax.ws.rs.HttpMethod.PUT;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
@@ -37,5 +43,22 @@ public class WebServices {
     String username = request.getHeader("X-user");
     return Response.ok(services.getWorld(username)).build();
     }
+    
+    @PUT
+    @Path("product")
+    @Consumes({MediaType.APPLICATION_JSON})
+    public Response editProduct(String data){
+        ProductType product = new Gson().fromJson(data, ProductType.class);
+        return Response.ok(services.updateProduct(data, product)).build();
+    }
+            
+    @PUT
+    @Path("manager")
+    @Consumes({MediaType.APPLICATION_JSON})
+    public Response editManager(String data){
+        PallierType manager = new Gson().fromJson(data, PallierType.class);
+        return Response.ok(services.updateManager(data, manager)).build();
+    }
+    
 
 }
